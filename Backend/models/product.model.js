1f1eefba169d1d0b20c85f15c8d449d494ba5766
bharathaladandi <bharathaladandi@gmail.com>
2023-02-16 23:57:45 +0530

    product model added

--- Backend/models/product.model.js
+++ Backend/models/product.model.js
@@ -1,6 +1,18 @@
 const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
+
     title:{type:String, require:true},
-    stock: {type:Number, default:190}
+    stock: {type:Number, default:190},
+    cost: {type:Number, require:true},
+    image: {type: String, require:true},
+    category: {type: String, default:"other"},
+    brand: {type: String, default:"other"}
+
+
 })
+
+
+const Productmodel = mongoose.model("product", productSchema);
+
+module.exports = { Productmodel}
