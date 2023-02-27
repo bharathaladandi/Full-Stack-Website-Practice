7763c906a7d136eca9711aa2ce569d5331c45c11
bharathaladandi <bharathaladandi@gmail.com>
2023-02-27 22:10:06 +0530

    cart route post method created and cart route completed

--- Backend/Routes/Cart.rotes.js
+++ Backend/Routes/Cart.rotes.js
@@ -17,4 +17,25 @@ app.get("/", async(req, res) => {
     } catch (error) {
         console.log(error.message);
     }
+
 });
+
+
+app.post("/", async(req, res) => {
+
+    try {
+
+        let cart = new Cart.create(req.body);
+
+        res.status(200).send(cart);
+
+        console.log("Post Cart Successfull");
+        
+    } catch (error) {
+        
+        console.log(error.message);
+    }
+
+});
+
+module.exports = app;
