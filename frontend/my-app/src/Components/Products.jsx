a09da05f8b9c6d1a8fdf1acac093bbe938e0195c
bharathaladandi <bharathaladandi@gmail.com>
2023-03-09 23:09:28 +0530

    Pagination Added

--- frontend/my-app/src/Components/Products.jsx
+++ frontend/my-app/src/Components/Products.jsx
@@ -1,48 +1,80 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+
+import { Link, useSearchParams } from "react-router-dom";
 
 
+const getCurrentPage = (val) => {
+
+  val = Number(val);
+
+  if (typeof val === "number" && val <= 0) {
+    val = 1;
+  }
+
+  if (!val) {
+    val = 1
+  }
+
+  return val
+}
 const getData = (url) => {
 
-return fetch(url).then((res) => res.json())
+  return fetch(url).then((res) => res.json())
 }
 export const Products = () => {
 
   const [data, setData] = useState([]);
 
-  const [page, setPage] = useState(1);
+  let [searchParams, setSearchParams] = useSearchParams();
 
-  
+  const [page, setPage] = useState(
+    getCurrentPage(searchParams.get("page")) || 1
+  );
+
   const limit = 5;
-  
+
   useEffect(() => {
 
-    getData(`https://blossombackend.onrender.com/products/Sale/asc?page=2&limit=${limit}`).then((res) => {
+    getData(`https://blossombackend.onrender.com/products/Sale/asc?page=${page}&limit=${limit}`).then((res) => {
 
-    console.log(res);
+      console.log(res);
       setData(res);
     })
-  }, [])
+  }, [page])
+
+
+  useEffect(() => {
+
+    let paramObj = { page };
+
+    setSearchParams(paramObj)
+  }, [page])
   return (
     <div
-     style={{
-      display: "grid",
-      gridTemplateColumns: "repeat(2,1fr)",
-      width: "80%",
-      margin: "auto",
-      gap: "20px"
-    }} >
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(2,1fr)",
+        width: "80%",
+        margin: "auto",
+        gap: "20px"
+      }} >
       {data?.map((item) => (
-        
+
         <div key={item._id}>
-          <img style={{alignItems:'center' ,height:'100px', width:'130px'}} src={item.image} alt="prof.img" />
+          <img style={{ alignItems: 'center', height: '100px', width: '130px' }} src={item.image} alt="prof.img" />
           <h6>{item.title}</h6>
           <h5> Price: {item.price}</h5>
         </div>
       ))}
 
-      <button disabled={page === 1} onClick={() => setPage(page -1)}>PREV</button>
-      <button>{page}</button>
-      <button onClick={() => setPage(page +1)}>NEXT</button>
+
+
+      <div>
+
+        <button disabled={page === 1} onClick={() => setPage(page - 1)}>PREV</button>
+        <button>{page}</button>
+        <button onClick={() => setPage(page + 1)}>NEXT</button>
+      </div>
     </div>
 
   )
