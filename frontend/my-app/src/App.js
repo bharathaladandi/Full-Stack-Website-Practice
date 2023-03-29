f73cae429978d91cd3e760152965739fc7f073ec
bharathaladandi <bharathaladandi@gmail.com>
2023-03-29 21:50:32 +0530

    color update

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -13,11 +13,15 @@ function App() {
 
     // #f7fafc
     // #f6f6f6
-    <div  className="App"style={{backgroundImage:"url('https://w0.peakpx.com/wallpaper/154/372/HD-wallpaper-telegram-background-whatsapp-creative-android-pattern-texture-abstract.jpg')", backgroundColor:'#f6f6f6'}} >
+
+    
+    <div  className="App"style={{backgroundImage:"url('https://w0.peakpx.com/wallpaper/154/372/HD-wallpaper-telegram-background-whatsapp-creative-android-pattern-texture-abstract.jpg')", backgroundColor:'#f7fafc'}} >
       <Navbar />
       <AllRoutes /> 
       <Footer />
     </div>
+
+
   );
 }
 
