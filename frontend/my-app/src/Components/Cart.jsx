a8e8aba11389993d88e62d2fd2fe4c4435beb913
bharathaladandi <bharathaladandi@gmail.com>
2023-03-31 21:52:33 +0530

    cart page old ui deleted

--- frontend/my-app/src/Components/Cart.jsx
+++ frontend/my-app/src/Components/Cart.jsx
@@ -35,7 +35,7 @@ import { CartOrderSummary } from '../Pages/Cart/CartOrderSummary';
 // import { Rating } from '../Pages/Products/Rating';
 // import { ProductGrid } from '../Pages/Products/ProductGrid';
 
-import { remove } from '../Redux/cartSlice';
+// import { remove } from '../Redux/cartSlice';
 
 export const Cart = () => {
 
@@ -52,86 +52,7 @@ export const Cart = () => {
 
   // }
   return (
-    // <div style={{margin:'auto', width:'80%'}}>
-
-    //   <h3>Cart</h3>
-
-    //   <div style={{marginTop:'80px'}}>
-    //   <ProductGrid>
-    //     {
-    //       products && products.map((product) => (
-
-    //         // <div>
-    //         //     <img src={product.image} alt="alt" />
-
-    //         //     <h5>{product.title}</h5>
-
-    //         //     <h5>{product.price}</h5>
-
-    //         //     <button>Remove</button>
-    //         // </div>
-
-    //         <Stack
-    //           spacing={{
-    //             base: '4',
-    //             md: '5',
-    //           }}
-    //         >
-    //           <Box position="relative">
-    //             <AspectRatio ratio={4 / 4}>
-    //               <Image
-    //                 src={product.image}
-    //                 alt={product.category}
-    //                 draggable="false"
-    //                 fallback={<Skeleton />}
-    //                 borderRadius={{
-    //                   base: 'md',
-    //                   md: 'xl',
-    //                 }}
-    //               />
-    //             </AspectRatio>
-    //             <FavouriteButton
-    //               position="absolute"
-    //               top="4"
-    //               right="4"
-    //               aria-label={`Add ${product.title} to your favourites`}
-    //             />
-    //           </Box>
-    //           <Stack>
-    //             <Stack spacing="1">
-    //               <Text fontWeight="medium" color={('gray.700', 'gray.400')}>
-    //                 {product.title}
-    //               </Text>
-    //               <PriceTag price={product.price} salePrice={product.price} currency="USD" />
-    //             </Stack>
-    //             <HStack>
-    //               <Rating defaultValue={product.rating} size="sm" />
-    //               <Text fontSize="sm" color={('gray.600', 'gray.400')}>
-    //                 12 Reviews
-    //               </Text>
-    //             </HStack>
-    //           </Stack>
-    //           <Stack align="center">
-    //             <Button onClick={() => handleRemove(product._id)} colorScheme="blue" width="full">
-    //               {/* Quick shop */}  Remove
-    //             </Button>
-    //             <Link
-    //               textDecoration="underline"
-    //               fontWeight="medium"
-    //               color={('gray.600', 'gray.400')}
-    //             >
-    //               {/* Quick shop */}
-    //             </Link>
-    //           </Stack>
-    //         </Stack>
-    //       ))
-    //     }
-    //     </ProductGrid>
-    //   </div>
-
-
-    // </div>
-
+  
     <div style={{ marginTop: '50px' }}>
       <Box
         maxW={{
